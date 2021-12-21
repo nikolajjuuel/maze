89583e9bb2fe00bfbffd97d6ec58c596214dddc8
Nikolaj Johansen <nikolaj.juuel@gmail.com>
2021-12-21 14:08:07 -0800

    top and right x,y fixed

--- react/src/Board.jsx
+++ react/src/Board.jsx
@@ -9,8 +9,7 @@ function Cell(x, y) {
     this.visited = false;
 }
 
-
-const draw = (arr) => arr.map((elms, i) => {
+const draw = (arr) => arr.map((elms) => {
     const { top, right, bottom, left, visited } = elms;
     const styles = [];
     if (top) {
@@ -36,17 +35,55 @@ const draw = (arr) => arr.map((elms, i) => {
     )
 })
 
-
-
 function visited(cell) {
     cell.visited = true;
 }
 
 function index(x, y, columns) {
-    if(x < 0 || y < 0 || x > columns - 1 || y > columns - 1){
-        return -1;
+    if (x < 0 || y < 0 || x > columns - 1 || y > columns - 1) {
+        return undefined;
     }
     return x + y * columns;
+}
+
+let i = 0;
+
+const checkNeighbour = (cell, grid, colums) => {
+    visited(cell);
+
+    console.log('cell',cell);
+
+    const { x, y } = cell
+    const neighbours = [];
+    const top = grid[index(y, x - 1, colums)];
+    const right = grid[index(y + 1, x, colums)];
+    const bottom = grid[index(y, x + 1, colums)];
+    const left = grid[index(y - 1, x, colums)];
+
+    console.log('check neighbour')
+
+    if (top && !top.visited) {
+        neighbours.push(top);
+    }
+    if (right && !right.visited) {
+        neighbours.push(right);
+    }
+    if (bottom && !bottom.visited) {
+        neighbours.push(bottom);
+    }
+    if (left && !left.visited) {
+        neighbours.push(left);
+    }
+
+    if (neighbours.length > 0) {
+        console.log(neighbours)
+        const randomNeighbour = Math.floor(Math.random() * neighbours.length);
+
+        checkNeighbour(neighbours[randomNeighbour], grid, colums);
+
+    } else {
+        return undefined;
+    }
 }
 
 
@@ -57,6 +94,7 @@ export default function Board(props) {
     const colums = Math.floor(size / width);
     const rows = Math.floor(size / width);
     const grid = [];
+    console.log('board function')
 
     for (let x = 0; x < rows; x++) {
         for (let y = 0; y < colums; y++) {
@@ -64,37 +102,8 @@ export default function Board(props) {
             grid.push(cells);
         }
     }
-    let current = grid[0];
 
-    visited(current);
-
-
-    const checkNeighbour = (cell) => {
-        const { x, y } = cell
-        const neighbours = [];
-        const top    = grid[index(x,     y - 1, colums)];
-        const right  = grid[index(x + 1, y,     colums)];
-        const bottom = grid[index(x,     y + 1, colums)];
-        const left   = grid[index(x - 1, y,     colums)];
-
-        if(top && !top.visited){
-            neighbours.push(top);
-        }
-        if(right && !right.visited){
-            neighbours.push(right);
-        }
-        if(bottom && !bottom.visited){
-            neighbours.push(bottom);
-        }
-        if(left && !left.visited){
-            neighbours.push(left);
-        }
-
-    }
-
-
-    checkNeighbour(current)
-
+    checkNeighbour(grid[0], grid, colums);
 
     const cells = draw(grid);
 
